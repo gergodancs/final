f3761e2a06d93e1390dea49f3c1aea51eb5b5dfe
gergodancs <dancs.gergely@gmail.com>
2022-03-17 10:57:46 +0100

    done

---
+++ src/BodyContent.jsx
@@ -0,0 +1,196 @@
+import React from "react";
+
+const BodyContent = () => {
+  return (
+    <div className="content">
+      <div className="frame__container">
+        <div className="frame">
+          <h3>1</h3>
+          <span className="name">Kurt Vonnegut</span>
+          <h1>Breakfast of Champions</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur ducimus inventore consequatur accusamus totam ab cumque
+            dignissimos, molestias maiores natus recusandae quis non rem neque
+            debitis error officiis provident obcaecati? Delectus recusandae
+            consequatur veniam id!
+          </p>
+        </div>
+        <div className="foot">
+          <span className="read">read more</span>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div className="frame__container">
+        <div className="frame">
+          <h3>1</h3>
+          <span className="name">Kurt Vonnegut</span>
+          <h1>Breakfast of Champions</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur ducimus inventore consequatur accusamus totam ab cumque
+            dignissimos, molestias maiores natus recusandae quis non rem neque
+            debitis error officiis provident obcaecati? Delectus recusandae
+            consequatur veniam id!
+          </p>
+        </div>
+        <div className="foot">
+          <span className="read">read more</span>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div className="frame__container">
+        <div className="frame">
+          <h3>1</h3>
+          <span className="name">Kurt Vonnegut</span>
+          <h1>Breakfast of Champions</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur ducimus inventore consequatur accusamus totam ab cumque
+            dignissimos, molestias maiores natus recusandae quis non rem neque
+            debitis error officiis provident obcaecati? Delectus recusandae
+            consequatur veniam id!
+          </p>
+        </div>
+        <div className="foot">
+          <span className="read">read more</span>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div className="frame__container">
+        <div className="frame">
+          <h3>1</h3>
+          <span className="name">Kurt Vonnegut</span>
+          <h1>Breakfast of Champions</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur ducimus inventore consequatur accusamus totam ab cumque
+            dignissimos, molestias maiores natus recusandae quis non rem neque
+            debitis error officiis provident obcaecati? Delectus recusandae
+            consequatur veniam id!
+          </p>
+        </div>
+        <div className="foot">
+          <span className="read">read more</span>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div className="frame__container">
+        <div className="frame">
+          <h3>1</h3>
+          <span className="name">Kurt Vonnegut</span>
+          <h1>Breakfast of Champions</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur ducimus inventore consequatur accusamus totam ab cumque
+            dignissimos, molestias maiores natus recusandae quis non rem neque
+            debitis error officiis provident obcaecati? Delectus recusandae
+            consequatur veniam id!
+          </p>
+        </div>
+        <div className="foot">
+          <span className="read">read more</span>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div className="frame__container">
+        <div className="frame">
+          <h3>1</h3>
+          <span className="name">Kurt Vonnegut</span>
+          <h1>Breakfast of Champions</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur ducimus inventore consequatur accusamus totam ab cumque
+            dignissimos, molestias maiores natus recusandae quis non rem neque
+            debitis error officiis provident obcaecati? Delectus recusandae
+            consequatur veniam id!
+          </p>
+        </div>
+        <div className="foot">
+          <span className="read">read more</span>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BodyContent;
---
+++ src/Nav.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div className="nav__container">
+      <div>
+        <span>New York Times Best Sellers</span>
+      </div>
+      <div className="button__nav">
+        <button className="hamburger">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
